454108ac9f5f99187ebde89383346755a8e8cbbc
Krzysztof Baraniecki <krzysztof.webyou@gmail.com>
2019-09-28 17:16:11 +0200

    styles header

--- wp-content/themes/szablon-webyou/js/main.js
+++ wp-content/themes/szablon-webyou/js/main.js
@@ -1,1 +1,19 @@
-alert('DUPA');
+var navbarSelector = '.navbar'
+var className = 'fixed-top'
+var hederTop = document.getElementById("header-top");
+navbar = document.querySelector(navbarSelector)
+offset = window.pageYOffset + navbar.getBoundingClientRect().top
+
+function scrollHandler () {
+    if (window.pageYOffset > offset) {
+        navbar.classList.add(className)
+        hederTop.style.marginBottom = `${navbar.offsetHeight}px`;
+    } else {
+        navbar.classList.remove(className)
+        hederTop.style.marginBottom = null;
+    }
+}
+
+scrollHandler()
+
+window.addEventListener('scroll', scrollHandler)
